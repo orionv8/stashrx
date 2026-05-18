9e815c69d632daeaa7ca29238c5a275ef6895577
orionv8 <orionv888@gmail.com>
2026-05-19 00:33:54 +0800

    Added files

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'stashRx-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
