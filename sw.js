be4be29c7e75e3d07442f4d7d9d3462419e8607b
Orion <orionv888@gmail.com>
2026-05-18 21:24:12 +0000

    Fix Android PWA installation fallback to shortcut with proper WebAPK manifest and ServiceWorker aggressive cleanup

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = 'stashRx-v3';
+const CACHE_NAME = 'stashRx-v4';
 const ASSETS = [
-  './',
-  './index.html',
-  './app.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './favicon.ico'
+  '/stashrx/',
+  '/stashrx/index.html',
+  '/stashrx/app.html',
+  '/stashrx/manifest.json',
+  '/stashrx/icon-192.png',
+  '/stashrx/icon-512.png',
+  '/stashrx/favicon.ico'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
@@ -17,9 +18,23 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
       return response || fetch(event.request);
     })
   );
